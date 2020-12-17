3b0cba25586ce49ffeb91bdb883d2045624956ec
BigEgg <cd_bigegg@hotmail.com>
2020-12-16 16:13:23 -0800

    Add remote image view

---
+++ src/iOS/BoardGameScorer/Views/Shared/RemoteImage.swift
@@ -0,0 +1,79 @@
+//
+//  RemoteImage.swift
+//  BoardGameScorer
+//
+//  Created by Jianming Xiao on 12/10/20.
+//
+
+import SwiftUI
+
+struct RemoteImage: View {
+    private enum LoadState {
+        case loading, success, failure
+    }
+
+    private class Loader: ObservableObject {
+        var data = Data()
+        var state = LoadState.loading
+
+        init(url: String) {
+            guard let parsedURL = URL(string: url) else {
+                fatalError("Invalid URL: \(url)")
+            }
+
+            URLSession.shared.dataTask(with: parsedURL) { data, response, error in
+                if let data = data, data.count > 0 {
+                    self.data = data
+                    self.state = .success
+                } else {
+                    self.state = .failure
+                }
+
+                DispatchQueue.main.async {
+                    self.objectWillChange.send()
+                }
+            }.resume()
+        }
+    }
+
+    @StateObject private var loader: Loader
+    var loading: Image
+    var failure: Image
+
+    var body: some View {
+        selectImage()
+            .resizable()
+    }
+
+    init(url: String, loading: Image = Image(systemName: "photo"), failure: Image = Image(systemName: "multiply.circle")) {
+        _loader = StateObject(wrappedValue: Loader(url: url))
+        self.loading = loading
+        self.failure = failure
+    }
+
+    private func selectImage() -> Image {
+        switch loader.state {
+        case .loading:
+            return loading
+        case .failure:
+            return failure
+        default:
+            if let image = UIImage(data: loader.data) {
+                return Image(uiImage: image)
+            } else {
+                return failure
+            }
+        }
+    }
+}
+
+struct RemoteImage_Previews: PreviewProvider {
+    static let imageUrl = "https://avatars1.githubusercontent.com/u/1243699?s=460&v=4"
+    
+    static var previews: some View {
+        RemoteImage(url: imageUrl)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200)
+            .previewLayout(.fixed(width: 300, height: 300))
+    }
+}
